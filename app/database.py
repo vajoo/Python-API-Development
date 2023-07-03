618bb0ec64bcf2884be48fda669c6a7baacdb783
vajoo <laurin.windhaus@gmail.com>
2023-07-03 16:26:50 +0200

    fixed error -> app doesnt start anymore -> result: reinstall requirements.txt

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
 # 'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
