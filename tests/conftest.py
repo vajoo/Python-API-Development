a395dda20328209107b1da3be196ed8bd711aed9
vajoo <laurin.windhaus@gmail.com>
2023-07-05 17:30:22 +0200

    added test cases for updating and deleting posts

--- tests/conftest.py
+++ tests/conftest.py
@@ -55,6 +55,16 @@ def test_user(client):
 
 
 @pytest.fixture
+def test_user2(client):
+    user_data = {"email": "laurin2@gmail.com", "password": "12345"}
+    res = client.post("/users/", json=user_data)
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    assert res.status_code == 201
+    return new_user
+
+
+@pytest.fixture
 def token(test_user):
     return create_access_token({"user_id": test_user['id']})
 
@@ -69,7 +79,7 @@ def authorized_client(client, token):
 
 
 @pytest.fixture
-def test_posts(test_user, session):
+def test_posts(test_user, test_user2, session):
     post_data = [
         {
             "title": "first title",
@@ -85,6 +95,11 @@ def test_posts(test_user, session):
             "title": "3rd title",
             "content": "3rd content",
             "owner_id": test_user['id']
+        },
+        {
+            "title": "4th title",
+            "content": "4th content",
+            "owner_id": test_user2['id']
         }
     ]
 
--- tests/test_posts.py
+++ tests/test_posts.py
@@ -57,3 +57,69 @@ def test_create_post_default_published_true(authorized_client, test_user):
 def test_unauthorized_user_create_post(client):
     res = client.post("/posts/", json={"title": "test title", "content":"test content"})
     assert res.status_code == 401
+
+
+def test_unauthorized_user_delete_post(client, test_posts):
+    res = client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_delete_post_success(authorized_client, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 204
+
+
+def test_delete_post_non_exist(authorized_client):
+    res = authorized_client.delete(f"/posts/88888888")
+    assert res.status_code == 404
+
+
+def test_delete_other_user_post(authorized_client, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[3].id}")
+    assert res.status_code == 403
+
+
+def test_update_post(authorized_client, test_posts):
+    post_data = {
+                    "title": "first title",
+                    "content": "first content",
+                    "owner_id": test_posts[0].id
+                }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=post_data)
+    updated_post = schemas.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == post_data['title']
+    assert updated_post.content == post_data['content']
+
+
+def test_update_other_user_post(authorized_client, test_posts):
+    post_data = {
+                    "title": "first title",
+                    "content": "first content",
+                    "owner_id": test_posts[3].id
+                }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=post_data)
+    assert res.status_code == 403
+
+
+def test_unauthorized_user_update_post(client, test_posts):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_update_post_non_exist(authorized_client, test_posts):
+    post_data = {
+                    "title": "first title",
+                    "content": "first content",
+                    "owner_id": test_posts[3].id
+                }
+    res = authorized_client.put(f"/posts/88888888", json=post_data)
+    assert res.status_code == 404
+
+
+
+
+
+
+
+# video - 17:07:50
