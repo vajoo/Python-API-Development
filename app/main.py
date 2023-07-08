866e017f720306a52ec52c73d68e2d9e48d5649e
vajoo <laurin.windhaus@gmail.com>
2023-07-08 19:05:39 +0200

    removed some stuff

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World! - ci/cd worked!!!"}
+    return {"message": "Hello World! - ci/cd worked!"}
 
 # TODO
 # - Tutorial finished
