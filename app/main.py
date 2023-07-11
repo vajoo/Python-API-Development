989e07ef9975e3ab1be46666081361ce7e7243af
vajoo <laurin.windhaus@gmail.com>
2023-07-11 21:28:25 +0200

    fixed bug in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from . import models
-from .database import engine
+from app import models
+from app.database import engine
 from .routers import post, user, auth, vote
 
 # models.Base.metadata.create_all(bind=engine) # sqlalchemy auto create tables if not exist
